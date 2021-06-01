77c1b22047ea819be345228255772aaec36cf537
Sony Wicaksono <aji.wicaksono@berkeley.edu>
2021-06-01 22:57:45 +0000

    Added EDA for captions and words count

---
+++ commons/utils.py
@@ -0,0 +1,19 @@
+__author__ = 'sony-w'
+__version__ = '1.0'
+
+ucb_palette = {
+    'berkeley_blue': '#003262',
+    'california_gold': '#FDB515',
+    'metallic_gold': '#BC9B6A',
+    'founders_rock': '#2D637F',
+    'medalist': '#E09E19',
+    'bay_fog': '#C2B9A7',
+    'lawrence': '#00B0DA',
+    'sather_gate': '#B9D3B6',
+    'pacific': '#53626F',
+    'soybean': '#9DAD33',
+    'california_purple': '#5C3160',
+    'south_hall': '#6C3302',
+    'stone_pine': '#584F29',
+    'ion': '#CFDD45'
+}
